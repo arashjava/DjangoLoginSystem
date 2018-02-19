2f9c333bca3680e0644325e8702d37a10bc6a7d7
Arash Shadmani <shadmaniarash@gmail.com>
2018-02-19 11:03:34 -0500

    fixing link errors and also removing password unnecessary messages

--- accounts/forms.py
+++ accounts/forms.py
@@ -36,6 +36,6 @@ class EditProfileForm(UserChangeForm):
         fields =(
                 'email',
                 'first_name',
-                'last_name',
-                'password'
+                'last_name'
+#                'password'
             )
--- accounts/urls.py
+++ accounts/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
     url(r'^register/$', views.register, name='register'),
     url(r'^profile/$', views.view_profile, name='view_profile'),
     url(r'^profile/edit/$', views.edit_profile, name='edit_profile'),
+    
+    url(r'^profile/password/$', views.change_password, name='change_password'),
     url(r'^change-password/$', views.change_password, name='change_password'),
     url(r'^reset-password/$', password_reset, name='reset_password'),
     url(r'^reset-password/done/$', password_reset_done, name='password_reset_done'),
